ed560a1bf0f9b72ea3eb960e246136b0ff3a8266
root <root@localhost.localdomain>
2026-04-23 23:00:30 +0800

    Fix maoning db connection kwargs syntax

--- maoning_api/db.py
+++ maoning_api/db.py
@@ -6,13 +6,13 @@ from pymysql.cursors import DictCursor
 
 def get_connection(config, dict_cursor=False):
     connect_kwargs = {
-        host=config["MYSQL_HOST"],
-        port=config["MYSQL_PORT"],
-        user=config["MYSQL_USER"],
-        password=config["MYSQL_PASSWORD"],
-        database=config["MYSQL_DATABASE"],
-        charset="utf8mb4",
-        autocommit=False,
+        "host": config["MYSQL_HOST"],
+        "port": config["MYSQL_PORT"],
+        "user": config["MYSQL_USER"],
+        "password": config["MYSQL_PASSWORD"],
+        "database": config["MYSQL_DATABASE"],
+        "charset": "utf8mb4",
+        "autocommit": False,
     }
     if dict_cursor:
         connect_kwargs["cursorclass"] = DictCursor
