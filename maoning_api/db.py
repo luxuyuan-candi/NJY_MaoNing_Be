7b8745a398b8cec42851526ea55ee6e996e179c0
root <root@localhost.localdomain>
2026-04-23 22:58:18 +0800

    Fix maoning DB writes and serve assets from minio ingress

--- maoning_api/db.py
+++ maoning_api/db.py
@@ -5,17 +5,18 @@ from pymysql.cursors import DictCursor
 
 
 def get_connection(config, dict_cursor=False):
-    cursor_class = DictCursor if dict_cursor else None
-    return pymysql.connect(
+    connect_kwargs = {
         host=config["MYSQL_HOST"],
         port=config["MYSQL_PORT"],
         user=config["MYSQL_USER"],
         password=config["MYSQL_PASSWORD"],
         database=config["MYSQL_DATABASE"],
         charset="utf8mb4",
-        cursorclass=cursor_class,
         autocommit=False,
-    )
+    }
+    if dict_cursor:
+        connect_kwargs["cursorclass"] = DictCursor
+    return pymysql.connect(**connect_kwargs)
 
 
 def json_ready(value):
